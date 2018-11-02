a084c9b793c1414870dc8f9f372d5001633ca7ba
mmarenzi <44713451+mmarenzi@users.noreply.github.com>
2018-11-02 16:47:48 -0700

    Update company.cpp

--- lab_7/Files/company.cpp
+++ lab_7/Files/company.cpp
@@ -117,7 +117,8 @@ namespace coen79_lab7
         else{
             node* cursor = this->head_ptr;
             while (cursor->getLink() != NULL){
-                if (std::strcmp(((cursor->getLink())->getName()), product_name) == 0){
+                const char* next = (cursor->getLink())->getName();
+                if (std::strcmp(next, product_name) == 0){
                     node * temp = this->head_ptr;
                     temp->setLink((cursor->getLink())->getLink());
                     cursor->setLink(temp);
